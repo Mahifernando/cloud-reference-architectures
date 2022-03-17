d7dfc23647d8b15b7797a5125a93ad422050de7c
Mahi Fernando <mahifernando@gmail.com>
2022-03-17 07:21:39 +0000

    bootstrapping lambda function for stack testing

---
+++ aws-liveness-detection/backend/liveness-app/lambda/challenge_service.py
@@ -0,0 +1,15 @@
+import json
+
+def handler(event, context):
+    print('{}'.format(json.dumps(event)))
+    response = { 'event_path' : event['path'], 'message': 'execute lambda function via api gateway' }
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST',
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps(response)
+    }
